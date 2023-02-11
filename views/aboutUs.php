9d4dd914190f1562a26fffe5cc934f3d60c92365
ardit <ab51944@ubt-uni.net>
2023-02-11 13:57:44 +0100

    Added about us page

--- views/aboutUs.php
+++ views/aboutUs.php
@@ -2,6 +2,7 @@
 <link rel="stylesheet" href="css/footerStyle.css" />
 <link rel="stylesheet" href="css/headersStyle.css" />
 <link rel="stylesheet" href="css/productStyle.css" />
+<link rel="stylesheet" href="css/aboutUs.css" />
 </head>
 
 <body>
@@ -12,10 +13,27 @@
     ?>
 
     <main>
-        <section class="aboutus-section">
-            <h2 class="title">About us</h2>
+    <div class="wrapper">
 
-        </section>
+        <div class="background-container">
+            <div class="bg-1"></div>
+            <div class="bg-2"></div>
+    
+        </div>
+        <div class="about-container">
+            
+            <div class="image-container">
+                <img src="./images/preview/desktop1.png" alt="">
+                
+            </div>
+
+            <div class="text-container">
+                <h1>About us</h1>
+                <p>Polar is your one-stop shop for all things computer. We are a leading provider of computers, monitors, laptops, and other computer accessories. Our goal is to provide our customers with the best possible products and services at the most competitive prices. We believe in delivering quality products and excellent customer service. Our team of experts is always on hand to help you with any questions or concerns you may have. Whether you are a home user looking for a new laptop or a business owner in need of a complete computer system, we are here to help.</p>
+            </div>
+            
+        </div>
+    </div>
 
     </main>
 
